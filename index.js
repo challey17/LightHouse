3b2c3686069cb4428d6da98a421b8821125e93d6
Chayce Knaub <chknaub@yahoo.com>
2020-02-12 01:54:40 -0500

    tested firt fetch successfully

--- index.js
+++ index.js
@@ -4,3 +4,32 @@
 function displayCurrentWeater() {}
 
 console.log(Date.now());
+
+//testing fetch api
+
+const lat = 27.7;
+const lng = -82.74;
+const currrentTime = Date.now();
+// put in source query source = sg
+const params = [
+  "airTemperature",
+  "windDirection",
+  "windSpeed",
+  "gust",
+  "waterTemperature"
+].join(",");
+
+fetch(
+  `https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}&params=${params}&time=${currrentTime}`,
+  {
+    headers: {
+      Authorization:
+        "db77f6ce-46f5-11ea-8bd6-0242ac130002-db77f7e6-46f5-11ea-8bd6-0242ac130002"
+    }
+  }
+)
+  .then(response => response.json())
+  .then(jsonData => {
+    // Do something with response data.
+    console.log(jsonData);
+  });
