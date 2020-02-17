d669e44dd471f5a0915b065a7d7f53d8133c6fb7
Chayce Knaub <chknaub@yahoo.com>
2020-02-16 23:38:02 -0500

    added reset btn

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
 "use strict";
 // ok here we go
 let results;
-/*function displayCurrentWeater(jsonData) {
-  //convert units from Json data//
-  let currentAirTemp =
-    jsonData.hours[currentTime].airTemperature[0].value * 2 + 32;
-  let currentWindDirection = "test";
-  if (jsonData.hours[currentTime].windDirection[0].value <= 90) {
-    currentWindDirection = "NE";
-  } else if (
-    jsonData.hours[currentTime].windDirection[0].value >= 91 &&
-    jsonData.hours[currentTime].windDirection[0].value <= 180
-  ) {
-    currentWindDirection = "SE";
-  } else if (
-    jsonData.hours[currentTime].windDirection[0].value >= 181 &&
-    jsonData.hours[currentTime].windDirection[0].value <= 270
-  ) {
-    currentWindDirection = "SW";
-  } else if (
-    jsonData.hours[currentTime].windDirection[0].value >= 271 &&
-    jsonData.hours[currentTime].windDirection[0].value <= 360
-  ) {
-    currentWindDirection = "NW";
-  }
 
-  let currentWindSpeed = jsonData.hours[currentTime].windSpeed[0].value * 2.237;
-
-  let currentGust = jsonData.hours[currentTime].gust[0].value * 2.237;
-
-  let currentWaterTemp =
-    jsonData.hours[currentTime].waterTemperature[0].value * 2 + 32;
-
-  //add to DOM
-  $("#current-weather-results").append(
-    `<li>Air Temp ${currentAirTemp}*F</li>
-    
-   <li>windDirection ${currentWindDirection}</li>
-   <li>windSpeed ${Math.round(currentWindSpeed)} mph</li>
-   <li>gust ${Math.round(currentGust)} mph</li>
-   <li>waterTemperature ${currentWaterTemp}*F</li>`
-  );
-}*/
 let currentTime = new Date().getUTCHours();
 
 console.log(currentTime);
 //submit event listener
 $("#user-selection").submit(function(event) {
   event.preventDefault();
+  //$("#selected-weather-results").remove();
   displaySelectedWeather(results);
 });
 
@@ -55,6 +16,9 @@ $(document).ready(function() {
   console.log($("#user-time").val());
   $("#user-time").change(function() {
     console.log($("option:selected", this).text());
+  });
+  $("#reset").on("click", function(event) {
+    $("#selected-weather-results").empty();
   });
 });
 
@@ -95,26 +59,24 @@ function displaySelectedWeather(jsonData, requestedTime) {
   //add to DOM
   if (requestedTime) {
     $("#current-weather-results").append(
-      `<li><h2>${$("#user-time option")
-        .eq(currentTime)
-        .text()}</h2></li>
+      `
       
-      <li>Air Temp ${selectedAirTemp}*F</li>
+      <li>Air Temp ${Math.round(selectedAirTemp)}*F</li>
       
      <li>windDirection ${selectedWindDirection}</li>
      <li>windSpeed ${Math.round(selectedWindSpeed)} mph</li>
      <li>gust ${Math.round(selectedGust)} mph</li>
-     <li>waterTemperature ${selectedWaterTemp}*F</li>`
+     `
     );
   } else {
     $("#selected-weather-results").append(
       `<li><h2>${$("option:selected", this).text()}</h2></li>
-    <li>Air Temp ${selectedAirTemp}*F</li>
+    <li>Air Temp ${Math.round(selectedAirTemp)}*F</li>
     
    <li>windDirection ${selectedWindDirection}</li>
    <li>windSpeed ${Math.round(selectedWindSpeed)} mph</li>
    <li>gust ${Math.round(selectedGust)} mph</li>
-   <li>waterTemperature ${selectedWaterTemp}*F</li>`
+   `
     );
   }
 }
@@ -144,10 +106,8 @@ fetch(
 )
   .then(response => response.json())
   .then(jsonData => {
-    // Do something with response data.
     results = jsonData;
     console.log(jsonData);
     console.log(jsonData.hours[currentTime].time);
     displaySelectedWeather(jsonData, currentTime);
-    //displayCurrentWeater(jsonData);
   });
